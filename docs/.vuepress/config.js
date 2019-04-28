d4076ff093c5a26bf5572d6a800a02fa9ed26520
Ybalabala0424 <39643766+Ybalabala0424@users.noreply.github.com>
2019-04-28 21:24:45 +0800

    VuePress-study

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title:'瑶九九der',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
